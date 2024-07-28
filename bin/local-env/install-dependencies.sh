0ce3c833ba1620d755a1aec5ec0fa9dfd67098f5
thelovekesh <lovekesh.kumar@rtcamp.com>
2024-07-28 20:08:32 +0530

    Install mailpit in appserver

--- bin/local-env/install-dependencies.sh
+++ bin/local-env/install-dependencies.sh
@@ -12,6 +12,8 @@ set -xe
 
 apt-get install ca-certificates curl gnupg -y
 
+curl -sL https://raw.githubusercontent.com/axllent/mailpit/develop/install.sh | bash
+
 mkdir -p /etc/apt/keyrings
 curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
 echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | tee /etc/apt/sources.list.d/nodesource.list
