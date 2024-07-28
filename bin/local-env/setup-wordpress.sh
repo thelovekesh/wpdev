11212e1f1005109d1af814b111ff342ba45b5350
thelovekesh <lovekesh.kumar@rtcamp.com>
2024-07-28 19:28:00 +0530

    Fix WP installation path

--- bin/local-env/setup-wordpress.sh
+++ bin/local-env/setup-wordpress.sh
@@ -7,7 +7,7 @@ fi
 
 set -ex
 
-if [[ ! -e "$LANDO_MOUNT/public/core-dev" ]]; then
+if [[ ! -e "$LANDO_MOUNT/wp" ]]; then
   git clone https://github.com/WordPress/WordPress.git --depth 1 --branch $WORDPRESS_TAG "$LANDO_MOUNT/wp"
   rm -rf "$LANDO_MOUNT/wp/.git"
 fi
