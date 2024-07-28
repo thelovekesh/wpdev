8d8417e7a0669c7c65f6dfd9f8690c6b9b3e2be0
thelovekesh <lovekesh.kumar@rtcamp.com>
2024-07-28 17:13:23 +0530

    Add script to install required deps in lando env

---
+++ bin/local-env/install-dependencies.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+if [[ -z "$LANDO_MOUNT" ]]; then
+  echo "Error: Must be run the appserver.";
+  exit 1
+fi
+
+cd "$(dirname "$0")"
+
+echo "Installing Dependencies"
+set -xe
+
+apt-get install ca-certificates curl gnupg -y
+
+mkdir -p /etc/apt/keyrings
+curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | tee /etc/apt/sources.list.d/nodesource.list
+apt-get update -y
+apt-get install nodejs -y
+
+apt-get install zip -y
+
+# Download phpunit pahr based on the major version.
+if [[ ! -e /usr/local/bin/phpunit ]]; then
+  curl -L https://phar.phpunit.de/phpunit-$PHPUNIT_MAJOR.phar -o /usr/local/bin/phpunit
+  chmod +x /usr/local/bin/phpunit
+fi
